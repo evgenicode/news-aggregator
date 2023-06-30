0483f04e475d5c810a74c6ebfd5894299f4d3a01
evmatv <evgenii.contact@gmail.com>
2023-06-30 23:26:44 +0300

    add news items

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import mockResponse from "./mockResopnse.json";
 import Link from "next/link";
+import Image from "next/image";
 
 // Production
 // const API_KEY = process.env.API_KEY;
@@ -23,23 +24,55 @@ const fetchTrending = async () => {
   return mockResponse;
 };
 
+interface NewsItemProps {
+  title: any;
+  description: any;
+  image: any;
+  url: any;
+}
+
+const NewsItem: React.FC<NewsItemProps> = ({
+  title,
+  description,
+  image,
+  url,
+}) => {
+  return (
+    <div className="my-3 bg-green-700 shadow-lg p-5 ">
+      <li className="pb-4">
+        <Image
+          src={image}
+          alt={title}
+          width={300}
+          height={300}
+          unoptimized={true}
+        />
+        <h1 className="text-2xl pb-1">{title}</h1>
+        <p className="pb-2">{description}</p>
+        <a href={url} target="_blank" rel="noopener noreferrer">
+          Read more
+        </a>
+      </li>
+    </div>
+  );
+};
+
 const newsFeed = async () => {
   const trendingNews = await fetchTrending();
   // console.log(trendingNews);
 
   return (
-    <div className="pl-5 pr-5 ">
+    <div className="md:container flex flex-col pl-5 pr-5">
       <h1 className="pb-8 text-2xl">This is the news feed</h1>
-      <h1 className="pb-8 text-2xl test-class">This is a TEST</h1>
       <ul role="list">
-        {trendingNews.articles.map((item: any) => (
-          <div className="my-3 bg-sky-950 shadow-lg">
-            <li className="pb-8" key={item.title}>
-              <h1 className="text-2xl pb-1">{item.title}</h1>
-              <p className="pb-2">{item.description}</p>
-              <Link href={item.url}>{item.url}</Link>
-            </li>
-          </div>
+        {trendingNews.articles.map((item) => (
+          <NewsItem
+            key={item.title}
+            title={item.title}
+            description={item.description}
+            image={item.image}
+            url={item.url}
+          />
         ))}
       </ul>
     </div>
