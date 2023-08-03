408b4778a629c4eec34f79f76d5a2a618dc899dd
evmatv <evgenii.contact@gmail.com>
2023-08-03 05:26:39 +0300

    add dropdown menu

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Link from "next/link";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className="md:relative md:container md:mx-auto p-3 bg-green-700">
       <div className="flex items-center justify-between">
@@ -23,6 +31,30 @@ const Header = () => {
         <button className="hidden md:block rounded-full bg-slate-300 p-2 px-5 pt-2 text-lime-900 hover:bg-black hover:text-white">
           Get Started
         </button>
+        <button
+          id="menu-btn"
+          className={`md:hidden block hamburger focus:outline-none ${
+            isOpen ? "open" : ""
+          }`}
+          onClick={handleMenuClick}
+        >
+          <span className="hamburger-top"></span>
+          <span className="hamburger-middle"></span>
+          <span className="hamburger-bottom"></span>
+        </button>
+      </div>
+      <div className={`md:hidden ${isOpen ? "dropdown.open" : "dropdown"}`}>
+        <div
+          id="menu"
+          className="absolute flex flex-col items-center self-end py-8 mt-10 space-y-6 
+          font-bold sm:w-auto sm:self-center left-6 right-6 drop-shadow-md bg-green-700"
+        >
+          <Link href="/">Pricing</Link>
+          <Link href="/">Product</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Careers</Link>
+          <Link href="/">Community</Link>
+        </div>
       </div>
     </header>
   );
